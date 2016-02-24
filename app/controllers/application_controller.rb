dfa7a84fd67df79f393b389434a121bf96603318
theory friction practice <jackson.brebnor@gmail.com>
2016-02-24 18:27:34 -0500

    sets up devise views and configures username auth key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-    # include MachineController
+  # include MachineController
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+  end
 end
