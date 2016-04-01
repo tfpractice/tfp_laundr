8bbdbfb76b26a7c545738e8159d799fc5c77931d
theory friction practice <jackson.brebnor@gmail.com>
2016-04-01 19:47:16 -0400

    adds inline documnetation for Dryer

--- app/models/dryer.rb
+++ app/models/dryer.rb
@@ -1,32 +1,45 @@
-# Dries the load belonging to the claimed user
+# Dries the load belonging to the claiming user
 class Dryer < ActiveRecord::Base
-  # belongs_to :user
   include Machine
 
+  # starts the cycle
+  # dries load for alooted period
   def start
     super
     load.dry!(period)
   end
-  def insert_coins(count=0)
+
+  # calls module #insert_coin
+  # adjusts period for every coin inserted
+  def insert_coins(count = 0)
     super
-    self.period += (5*count.to_i)
-  end
-  private
-  def set_name
-    self.update(name: "Dryer ##{self.id}" )  unless self.name
-  end
-  def set_instance_attributes
-    @price ||= 1
-    @capacity ||= 15.0
-    @period ||=  5 * self.coins
+    self.period += (5 * count.to_i)
   end
 
   private
-  # Dryer.workflow_spec.states[:ready].events.push(:insert_coins,(Workflow::Event.new(:insert_coins, :ready)))
+
+  # sets name attribute based on db id
+  def set_name
+    self.update(name: "Dryer ##{self.id}") unless self.name
+  end
+
+  # sets common attributes for Dryer
+  # @!attribute price = 1
+  # @!attribute capacity = 15
+  # @!attribute period = 5*coins
+  def set_instance_attributes
+    @price ||= 1
+    @capacity ||= 15.0
+    @period ||= 5 * self.coins
+  end
+
+  # As dryers can receive coins during more states
+  # than washers, this  method adds #insert_coins
+  # as a possible event to these states
   def self.append_workflow
-    self.workflow_spec.states[:ready].events.push(:insert_coins,(Workflow::Event.new(:insert_coins, :ready)))
-    self.workflow_spec.states[:in_progress].events.push(:insert_coins,(Workflow::Event.new(:insert_coins, :in_progress)))
-    self.workflow_spec.states[:complete].events.push(:insert_coins,(Workflow::Event.new(:insert_coins, :ready)))
+    self.workflow_spec.states[:ready].events.push(:insert_coins, Workflow::Event.new(:insert_coins, :ready))
+    self.workflow_spec.states[:in_progress].events.push(:insert_coins, Workflow::Event.new(:insert_coins, :in_progress))
+    self.workflow_spec.states[:complete].events.push(:insert_coins, Workflow::Event.new(:insert_coins, :ready))
   end
   append_workflow
 end
