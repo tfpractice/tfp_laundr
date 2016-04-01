725d318f55093913f7c7c3f57accc2a082a52ad0
theory friction practice <jackson.brebnor@gmail.com>
2016-04-01 19:37:27 -0400

    more inline doc of machine module

--- app/models/concerns/machine.rb
+++ app/models/concerns/machine.rb
@@ -18,9 +18,9 @@ module Machine
     # named scopes for relationships
     scope :available_machines, -> { where(state: 'available') }
     scope :affordable_machines, ->(user) { where(id: all.select { |machine| machine.price <= user.coins }.map(&:id)) }
-    scope :completed_machines, -> { where(state: 'complete')}
+    scope :completed_machines, -> { where(state: 'complete') }
     scope :unavailable_machines, -> { where.not(state: 'available') }
-    scope :can_accept_load, ->(load){ where("capacity >= ?", load.weight) }
+    scope :can_accept_load, ->(load) { where("capacity >= ?", load.weight) }
 
     include Workflow
     # avaialble states, transitions, and events
@@ -111,7 +111,7 @@ module Machine
       if (val.any? { |v| v.condition == nil })
         valid_events << event.id2name
       elsif (val.any? { |v| v.condition != nil })
-        valid_conditional_events = val.select { |v| v.condition}.select { |e|  e.condition.call(self) == true  }
+        valid_conditional_events = val.select { |v| v.condition }.select { |e| e.condition.call(self) == true }
         valid_conditional_events.each { |e| valid_events << event.id2name  }
       end
     end
@@ -128,7 +128,7 @@ module Machine
     begin
       user.reduce_coins(count.to_i)
       increment!(:coins, count.to_i)
-      halt! "machine cannot start until #{self.price} coins are inserted, currently has #{self.coins}" unless enough_coins?
+      halt! "machine cannot start until #{self.price} coins are inserted. currently has #{self.coins}" unless enough_coins?
     rescue Workflow::TransitionHalted => e
       errors.add(:coins, e)
     end
@@ -174,7 +174,7 @@ module Machine
 
   # calculates time_remaining
   def time_remaining
-    if self.state == "in_progress"
+    if self.state == 'in_progress'
       (Time.now - self.end_time).to_s
     else
       return @period
@@ -183,17 +183,17 @@ module Machine
 
   # @!attribute capacity
   def capacity
-    raise "Subclass responsibility"
+    raise 'Subclass responsibility'
   end
 
   # @!attribute period
   def period
-    raise "Subclass responsibility"
+    raise 'Subclass responsibility'
   end
 
   # @!attribute price
   def price
-    raise "Subclass responsibility"
+    raise 'Subclass responsibility'
   end
 
   #sets capacity, period, and price attributes
