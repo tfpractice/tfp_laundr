7ad3253000e4d5b826af6e201253e224c7d7e8c5
theory friction practice <jackson.brebnor@gmail.com>
2016-04-01 21:13:36 -0400

    adjusts inline docs for Ability Model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,11 @@
 # Use CanCanCan to manage user permissions.
-# Due to the DraperGems return type [Decorator], permissions are set on Decorators where applied, and not onto the decorated objects themselves.
-# In the case that Draper is removed, this behavior is backed up by adding permissions onto the objects themselves.
-
+# Due to the DraperGems return type [Decorator],
+# permissions are set on Decorators where applied,
+# and not onto the decorated objects themselves.
+# In the case that Draper is removed,
+# this behavior is backed up by adding permissions
+# onto the objects proper.
 class Ability
-
-
   include CanCan::Ability
 
   # admin users can :manage all records
@@ -13,7 +14,6 @@ class Ability
 
   # @param user [User] the current user or guest user
   # @return [CanCan::Ability] ability based on user role
-
   def initialize(user)
     user ||= User.new
     alias_action  :claim, :fill, :unclaim, :insert_coins, :return_coins, :start,  :remove_clothes, :to => :use
@@ -21,19 +21,14 @@ class Ability
       can :manage, :all
     elsif user.new_record?
       can :read, :all
-
     else
-
       can :use, [Washer, Dryer], user: user
       can :use, [Washer, Dryer], state: "available"
       can :use, MachineDecorator, state: "available"
       can :use, MachineDecorator, user: user
-
       can :manage, Load, user: user
       can :manage, LoadDecorator, user: user
       can :read, :all
     end
-
   end
-
 end
